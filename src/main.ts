fb4dcacacfb2a9909e3d1f59a72df7da66316836
inkomomutane <Nelson.mutane@uzambeze.ac.mz>
2025-11-26 00:30:34 +0200

    base

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,37 @@
-import { ViteSSG } from 'vite-ssg/single-page'
+import {ViteSSG} from 'vite-ssg/single-page'
 import i18next from "i18next";
 import I18NextVue from "i18next-vue";
 import 'flowbite'
 import './style.css'
 import App from './App.vue';
-import { en } from './locales/en';
-import { pt } from './locales/pt';
+import {en} from './locales/en';
+import {pt} from './locales/pt'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
+import Particles from "@tsparticles/vue3";
+//import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
+import {loadSlim} from "@tsparticles/slim"; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 
 const locales = {
-  en : {
-    translation :en
-  },
-  pt : {
-    translation :pt
-  },
+    en: {
+        translation: en
+    },
+    pt: {
+        translation: pt
+    },
 };
 
 i18next.init({
-  lng : 'en',
-  resources : locales
- });
+    lng: 'en',
+    resources: locales
+});
 
 // export const createApp = ViteSSG(App);
 
- export const createApp =  ViteSSG(App,({app}) => {
-    app.use(I18NextVue, { i18next });
+export const createApp = ViteSSG(App, ({app}) => {
+    app.use(I18NextVue, {i18next})
+        .use(Particles, {
+            init: async engine => {
+                // await loadFull(engine); // you can load the full tsParticles library from "tsparticles" if you need it
+                await loadSlim(engine); // or you can load the slim version from "@tsparticles/slim" if don't need Shapes or Animations
+            },
+        });
 });
