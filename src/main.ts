514a942da0bd1c2741e8e92ca45358ad9e1a15f4
inkomomutane <Nelson.mutane@uzambeze.ac.mz>
2025-11-26 09:40:26 +0200

    new UI

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,6 @@ import './style.css'
 import App from './App.vue';
 import {en} from './locales/en';
 import {pt} from './locales/pt'; // if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
-// import Particles from "@tsparticles/vue3";
-// import { loadFull } from "tsparticles"; // if you are going to use `loadFull`, install the "tsparticles" package too.
-// import {loadSlim} from "@tsparticles/slim";
-// if you are going to use `loadSlim`, install the "@tsparticles/slim" package too.
 const locales = {
     en: {
         translation: en
@@ -24,14 +20,7 @@ i18next.init({
     resources: locales
 });
 
-// export const createApp = ViteSSG(App);
-
 export const createApp = ViteSSG(App, ({app}) => {
     app.use(I18NextVue, {i18next})
-        // .use(Particles, {
-        //     init : async  ( engine : any) => {
-        //         // await loadFull(engine); // you can load the full tsParticles library from "tsparticles" if you need it
-        //         await loadFull(engine); // or you can load the slim version from "@tsparticles/slim" if don't need Shapes or Animations
-        //     },
-        // });
+
 });
