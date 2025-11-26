8b791d11a9df0b48869d91a4bda90bf0aa3f48d9
inkomomutane <Nelson.mutane@uzambeze.ac.mz>
2025-11-26 10:15:37 +0200

    update

--- src/App.vue
+++ src/App.vue
@@ -133,14 +133,14 @@ onMounted(() => {
            </Avatar>
 
             <h1 class="text-5xl font-bold text-white  font-semibold text-2xl leading-[100%] font-['Lexend_Deca'] mt-4 ">Nelson Alexandre Mutane,</h1>
-           <h2 class="text-xl font-semibold text-white ">IT + Software Engineer + Accounting learner @ Consul, Lda 🚀</h2>
+            <h2 class="text-xl font-semibold text-white ">IT + Software Engineer + Accounting learner @ Consul, Lda 🚀</h2>
 
            <p class="ms-0  text-md text-slate-300 mt-6 max-w-lg">
              Hi there! I'm Nelson Alexandre Mutane, IT, Software Engineer with more than 6 years of experience in software development from Mozambique.
            </p>
 
             <div class="mt-6 flex gap-4">
-              <a href="https://linkedin/in/nelson-mutane" class="group -m-1 p-1 animate-fade-up animate-delay-[300ms]">
+              <a href="https://linkedin.com/in/nelson-mutane" class="group -m-1 p-1 animate-fade-up animate-delay-[300ms]">
                 <svg viewBox="0 0 24 24" aria-hidden="true" class="size-6 fill-white/50 transition group-hover:fill-white/80"><path d="M18.335 18.339H15.67v-4.177c0-.996-.02-2.278-1.39-2.278-1.389 0-1.601 1.084-1.601 2.205v4.25h-2.666V9.75h2.56v1.17h.035c.358-.674 1.228-1.387 2.528-1.387 2.7 0 3.2 1.778 3.2 4.091v4.715zM7.003 8.575a1.546 1.546 0 01-1.548-1.549 1.548 1.548 0 111.547 1.549zm1.336 9.764H5.666V9.75H8.34v8.589zM19.67 3H4.329C3.593 3 3 3.58 3 4.297v15.406C3 20.42 3.594 21 4.328 21h15.338C20.4 21 21 20.42 21 19.703V4.297C21 3.58 20.4 3 19.666 3h.003z"></path></svg>
               </a>
               <a href="https://github.com/inkomomutane/" class="group -m-1 p-1 animate-fade-up animate-delay-[300ms]">
